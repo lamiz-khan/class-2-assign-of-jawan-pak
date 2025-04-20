4a7540bcbd75fd7563a321ff4b60a2ad2585a8aa
lamiz-khan <khanlamiz8@gmail.com>
2025-04-20 14:38:18 +0500

    finding area of rectangle

--- class 2.dart
+++ class 2.dart
@@ -13,4 +13,16 @@ print("Celsius to Fahrenheit");
 double Celsius = (Fahrenheit - 32) * 5/9;
 
 print (Celsius);
+print ("");
+
+print ("assignment 2");
+ print ("");
+ int length = 5;
+ int breadth = 7;
+ String area_of_rectangle = "?";
+ 
+ int area = length * breadth;
+ 
+ print (area);
+ print ("");
 }
